b06e86c0e80d8993cf0ca6a2951f934bb56a5c4e
Artem Lugovkin <artem-lugovkin@mail.ru>
2023-01-07 23:02:24 +0500

    DZ 2

--- src/Car.java
+++ src/Car.java
@@ -8,21 +8,19 @@ public class Car {
 
     String bodyСolor;
 
-    String yearOfProduction;
+    int yearOfProduction;
 
     String countryOfAssembly;
 
-    Car(String mark, String model, String engineСapacity, String bodyСolor, String yearOfProduction, String countryOfAssembly) {
+
+
+    public Car(String mark, String model, String engineСapacity, String bodyСolor, int yearOfProduction, String countryOfAssembly) {
         this.mark = mark;
         this.model = model;
         this.engineСapacity = engineСapacity;
         this.bodyСolor = bodyСolor;
         this.yearOfProduction = yearOfProduction;
         this.countryOfAssembly = countryOfAssembly;
-    }
-    void carInformation() {
-        System.out.println( mark + " " + model + ", " + yearOfProduction + " год выпуска, " + " сборка в " + engineСapacity  + ", " + bodyСolor + " цвета," + " обьем двигателя" + countryOfAssembly);
-
     }
 
 }
--- src/People.java
+++ src/People.java
@@ -6,36 +6,12 @@ public class People {
     String city;
     String post;
 
-
-
-
-
-    People maksim = new People();
-    maksim.name = "Максим";
-    maksim.age = 35;
-    maksim.city = "Минск";
-    maksim.post = "бренд-менеджер";
-
-
-    People any = new People();
-    any.name = "Аня";
-    any.age = 29;
-    any.city = "Москва";
-    any.post = "методист образовательных программ";
-
-    People kate = new People();
-    kate.name = "Катя";
-    kate.age = 28;
-    kate.city = "Калиниград";
-    kate.post = "продакт-менеджер";
-
-    People artem = new People();
-    artem.name = "Артём";
-    artem.age = 27;
-    artem.city = "Москва";
-    artem.post = "директор по развитию бизнеса";
-
-    void say() {
-        System.out.println("Привет! Меня зовут" + name + " Я из города " + city + "Мне " + age + " лет. Я работаю на должности" + post + " Будем знакомы!");
+    public People(String name, int age, String city, String post) {
+        this.name = name;
+        this.age = age;
+        this.city = city;
+        this.post = post;
     }
+
+
 }
